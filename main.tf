224687aedaee6cc57473aa70ecaebc9d5a9e5737
yuk1ty <yuki.mul.tiplus@gmail.com>
2021-06-07 23:36:36 +0900

    not to use tabs

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 provider "aws" {
-	region = "ap-northeast-1"
-	profile = "goodjob-cloud"
+  region = "ap-northeast-1"
+  profile = "goodjob-cloud"
 }
 
 resource "aws_vpc" "main" {
-	cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
 
-	tags = {
-		Name = "VPC領域"
-	}
+  tags = {
+    Name = "VPC領域"
+  }
 }
 
 resource "aws_subnet" "public_subnet" {
-	vpc_id = "${aws_vpc.main.id}"
+  vpc_id = "${aws_vpc.main.id}"
   cidr_block = "10.0.1.0/24"
   tags = {
     Name = "パブリックサブネット"
