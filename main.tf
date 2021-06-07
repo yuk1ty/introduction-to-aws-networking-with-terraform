89a11f716ffbeff4ec2d14ad69bbc2534858d5b1
yuk1ty <yuki.mul.tiplus@gmail.com>
2021-06-08 00:04:27 +0900

    add route table and others

--- main.tf
+++ main.tf
@@ -22,3 +22,21 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "パブリックルートテーブル"
+  }
+}
+
+resource "aws_route" "public" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public.id
+}
