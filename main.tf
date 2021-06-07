7bed5ee3e41bd12757c4f28913815d26a25e69c0
yuk1ty <yuki.mul.tiplus@gmail.com>
2021-06-07 23:34:47 +0900

    first commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+	region = "ap-northeast-1"
+	profile = "goodjob-cloud"
+}
+
+resource "aws_vpc" "main" {
+	cidr_block = "10.0.0.0/16"
+
+	tags = {
+		Name = "VPC領域"
+	}
+}
+
+resource "aws_subnet" "public_subnet" {
+	vpc_id = "${aws_vpc.main.id}"
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "パブリックサブネット"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+}
