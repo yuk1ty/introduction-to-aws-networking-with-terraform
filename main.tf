a165434173119f47279b3bbb695e111bad3e60f6
yuk1ty <yuki.mul.tiplus@gmail.com>
2021-06-12 09:25:55 +0900

    enable dns hostname

--- main.tf
+++ main.tf
@@ -4,7 +4,8 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
 
   tags = {
     Name = "VPC領域"
