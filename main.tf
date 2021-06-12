880d20de4ff7370d8173aeaacd30f653718fe9bf
yuk1ty <yuki.mul.tiplus@gmail.com>
2021-06-12 18:51:56 +0900

    add icmp ingress to web

--- main.tf
+++ main.tf
@@ -84,6 +84,13 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
