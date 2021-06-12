9ba1b498689801f0c81556925a1ecc1f1a2a888a
yuk1ty <yuki.mul.tiplus@gmail.com>
2021-06-12 12:17:26 +0900

    add private route table

--- main.tf
+++ main.tf
@@ -13,10 +13,22 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1c" # not necessary; for the sake of suppressing costs
+
   tags = {
     Name = "パブリックサブネット"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-northeast-1c"
+
+  tags = {
+    Name = "プライベートサブネット"
   }
 }
 
@@ -31,6 +43,13 @@ resource "aws_route_table" "public" {
   }
 }
 
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "プレイベートルートテーブル"
+  }
+}
+
 resource "aws_route" "public" {
   destination_cidr_block = "0.0.0.0/0"
   route_table_id         = aws_route_table.public.id
@@ -42,6 +61,10 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private.id
+}
 
 resource "aws_security_group" "web_sg" {
   name   = "WEB-SG"
